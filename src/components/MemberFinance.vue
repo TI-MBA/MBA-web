9946713b77ec889b56d16402eba754b37542623e
Gabriel Rosa <gabrielrosadn@gmail.com>
2022-12-28 09:13:04 -0300

    Add payment card view

--- src/components/MemberFinance.vue
+++ src/components/MemberFinance.vue
@@ -1,21 +1,10 @@
 <template>        
-    <div class="content">        
-        <table class="table is-borderless is-striped center mt-4">
-            <thead>
-                <tr>
-                    <th>Data referência</th>
-                    <th>valor</th>
-                    <th>observações</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="payment in paymentList" :key="payment.id">
-                    <td class="is-vcentered">{{ paymentDateText(payment.date) }}</td>
-                    <td class="is-vcentered">{{ "R$ " + (payment.paymentValue).toFixed(2) }}</td>
-                    <td class="is-vcentered width-limit"> {{ payment.observation }} </td>
-                </tr>
-            </tbody>
-        </table>
+    <div class="content">
+        <div class="container padding-horizontal">
+            <div v-for="payment in paymentList " :key="payment.id">
+                     <PaymentCard :payment="payment" />
+            </div>
+        </div>
         <br/>
         <progress class="progress is-small is-info" max="100" v-bind:class="progressVisibility">50%</progress>
     </div>
@@ -23,9 +12,11 @@
  
  <script>
  import axios from 'axios'
+ import PaymentCard from './PaymentCard.vue'
  
  export default {
     components: {
+        PaymentCard
     },
     created () {
         if (this.userId) {
@@ -62,14 +53,6 @@
             return paymentList.sort(function(a,b){
                 return new Date(b.date) - new Date(a.date);
             });
-        },
-        presenceDateText(dateString) {
-            const rehearsalDate = new Date(dateString);
-            return rehearsalDate.getDate() + "/" + (rehearsalDate.getUTCMonth() + 1) + "/" + rehearsalDate.getUTCFullYear()
-        },
-        paymentDateText(dateString) {
-            const paymentDate = new Date(dateString);
-            return (paymentDate.getUTCMonth() + 1) + "/" + (paymentDate.getUTCFullYear())
         }
     }
  }
@@ -82,4 +65,8 @@
      grid-gap: 1.5em;
      margin-top: 1.5em;
  }
+ .padding-horizontal {
+    padding-inline-start: 5%;
+    padding-inline-end: 5%;
+  }
  </style>
---
+++ src/components/PaymentCard.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="card">
+        <header class="card-header">
+            <p class="card-header-title">
+                Mês de referência:
+                {{ paymentDateText(payment.date) }}
+            </p>
+        </header>
+
+        <div class="card-content">
+            <div class="content">
+                <p class="has-text-left"> <b>Valor:</b><br/>{{ "R$ " + (payment.paymentValue).toFixed(2) }}</p>
+                <p class="has-text-left"> <b>Data do pagamento:</b><br/> {{ '10/12/2022' }}</p>
+                <p class="has-text-left"> <b>Observações:</b><br/> {{ payment.observation == '' ? '-' : payment.observation }} </p>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+
+export default {
+    props: {
+        payment: Object
+    },
+    data() {
+        return {
+        }
+    },
+    computed: {},
+    methods: {
+        paymentDateText(dateString) {
+            const paymentDate = new Date(dateString);
+            return (paymentDate.getUTCMonth() + 1) + "/" + (paymentDate.getUTCFullYear())
+        }
+    }
+}
+</script>
+
+<style>
+</style>
